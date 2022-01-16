3c2aa2704ba18e63692b47a794be465ef1828fb3
sd <dmitriy-stupin@mail.ru>
2022-01-16 21:43:33 +0300

    End of Section 12

--- Client/src/app/shop/product-details/product-details.component.ts
+++ Client/src/app/shop/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
 
 
@@ -12,7 +13,10 @@ import { ShopService } from '../shop.service';
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
-  constructor(private ShopService: ShopService, private activateRoute: ActivatedRoute) { }
+  constructor(private ShopService: ShopService, private activateRoute: ActivatedRoute,
+    private bcService: BreadcrumbService) {
+      this.bcService.set('@productDetails', ' ')
+     }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -21,6 +25,8 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct(){
     this.ShopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(product => {
       this.product = product;
+      this.bcService.set('@productDetails', product.name);
+      // вместо id продукта отображается имя продукта в хлебных крошках
     }, error => {
       console.log(error);
     });
