b338e8759932c8f69e518efe9aa19478dcfc19a2
sd <dmitriy-stupin@mail.ru>
2022-01-22 11:21:00 +0300

    End of section 14

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { BasketService } from './basket/basket.service';
 
 
 @Component({
@@ -11,10 +12,18 @@ export class AppComponent implements OnInit{
   title = 'NosokFun';
 
 
-  constructor(private http: HttpClient) {}
+  constructor(private basketService: BasketService) {}
 
   ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if (basketId) {
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('initialised basket');
+      }, error => {
+        console.log(error);
+      });
 
+    }
   }
 
 }
