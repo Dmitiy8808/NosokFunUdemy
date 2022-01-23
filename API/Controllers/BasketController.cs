62f8821a0fd87897585ca1f5d77c6501d2a651eb
sd <dmitriy-stupin@mail.ru>
2022-01-23 17:24:07 +0300

    End of section 16

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -13,9 +15,11 @@ namespace API.Controllers
     public class BasketController: BaseApiContriller
     {
         private readonly IBasketRepository _basketRepository;
+        private readonly IMapper _mapper;
 
-        public BasketController(IBasketRepository basketRepository)
+        public BasketController(IBasketRepository basketRepository, IMapper mapper)
         {
+            _mapper = mapper;
             _basketRepository = basketRepository;
         }
 
@@ -23,13 +27,17 @@ namespace API.Controllers
         public async Task<ActionResult<CustomerBasket>> GetBasketById (string id)
         {
             var basket = await _basketRepository.GetBasketAsync(id);
+            
             return Ok(basket ?? new CustomerBasket(id));
         }
 
         [HttpPost]
-        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasketDto basket)
         {
-            var updatedBasket = await _basketRepository.UpdateBasketAsync(basket);
+            var customerBasket = _mapper.Map<CustomerBasketDto, CustomerBasket>(basket);
+
+            var updatedBasket = await _basketRepository.UpdateBasketAsync(customerBasket);
+
             return Ok(updatedBasket);
         }
 
