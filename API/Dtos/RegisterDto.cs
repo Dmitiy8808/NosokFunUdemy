be89a01f1625c1c1b358367fa44d2c67f73979a2
sd <dmitriy-stupin@mail.ru>
2022-01-29 09:45:37 +0300

    End of section 17

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -14,7 +14,7 @@ namespace API.Dtos
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$", 
+        [RegularExpression("(?-i)(?=^.{8,}$)((?!.*\\s)(?=.*[A-Z])(?=.*[a-z]))(?=(1)(?=.*\\d)|.*[^A-Za-z0-9])^.*$", 
         ErrorMessage = "Password must have 1 Uppercase, 1 Lowercase, 1 Number, 1 Non alphanumeric, at least 6 characters ")]
         public string Password { get; set; }
     }
--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 
@@ -12,9 +13,23 @@ export class AppComponent implements OnInit{
   title = 'NosokFun';
 
 
-  constructor(private basketService: BasketService) {}
+  constructor(private basketService: BasketService, private accountSrvice: AccountService) {}
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser(){
+    const token = localStorage.getItem('token');
+      this.accountSrvice.loadCurrentUser(token).subscribe(() => {
+        console.log('loaded user');
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  loadBasket(){
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
       this.basketService.getBasket(basketId).subscribe(() => {
